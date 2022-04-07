a10344925fea7b91cc1e79d5ce7881a3dd9760c5
Sam <stomelleri@gus.pittstate.edu>
2022-04-06 20:18:01 -0500

    Pushing it all

---
+++ IntermediateProgramming/Lab7InsuranceUI/src/main/java/com/mycompany/lab7insuranceui/InvalidAgeException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab7insuranceui;
+
+/**
+ *
+ * @author sam13
+ */
+public class InvalidAgeException extends Exception{
+    public InvalidAgeException(String errorMessage) {
+        super(errorMessage);
+    }
+}
